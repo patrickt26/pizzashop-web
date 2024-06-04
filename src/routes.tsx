f7362e0a05b2f69e1538c550252f3421abfa9327
Patrick Thomaz <plthomaz@gmail.com>
2024-06-04 16:41:04 -0300

    [02] Página: Cadastro de restaurante

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
 import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
+import { SignUp } from "./pages/auth/sign-up";
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +24,10 @@ export const router = createBrowserRouter([
       {
         path: "/sign-in",
         element: <SignIn />,
+      },
+      {
+        path: "/sign-up",
+        element: <SignUp />,
       },
     ],
   },
